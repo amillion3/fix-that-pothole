bae280f54849d9577f60e70436ee39a0e879eec0
Andy <amillion3@gmail.com>
2018-07-05 14:25:30 -0500

    setup complete

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import {SimpleButton} from '@terrestris/react-geo';
-import '../../node_modules/antd/dist/antd.css';
-// import '../../node_modules/react-geo.css';
+
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <SimpleButton onClick={() => { alert('hi'); }}>
-        Hello World!
-      </SimpleButton>
+      <div className="App">
+        <h1>hello</h1>
+      </div>
     );
   }
 }
