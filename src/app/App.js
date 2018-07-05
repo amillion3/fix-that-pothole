0ce78fca316960985c64c5c4312466fec5150218
Andy <amillion3@gmail.com>
2018-07-04 22:42:07 -0500

    default setup complete, now to install react-geo

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <button className='btn btn-info'>Check check, 1 2 3...</button>
+    );
+  }
+}
+
+export default App;
