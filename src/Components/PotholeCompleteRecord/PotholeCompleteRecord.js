3f4530e53c8c36a59264500bb95917695950a583
Andy <amillion3@gmail.com>
2018-07-21 13:41:32 -0500

    WIP, save point while i adjust Dashboard component

--- src/Components/PotholeCompleteRecord/PotholeCompleteRecord.js
+++ src/Components/PotholeCompleteRecord/PotholeCompleteRecord.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import potholeRequests from '../../firebaseRequests/potholeRequests';
+
 import './PotholeCompleteRecord.css';
 
 class PotholeCompleteRecord extends React.Component {
+  state = {
+
+  }
+  componentDidMount () {
+    const firebaseId = this.props.match.params.id;
+    potholeRequests
+      .potholeGETSingle(firebaseId)
+      .then(response => {
+        console.error('response', response);
+        this.setState(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render () {
     return (
-      <div>single record</div>
+      <div>
+        <h2>{this.state.status}</h2>
+      </div>
     );
   }
 }
