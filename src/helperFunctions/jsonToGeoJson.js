c162a1246e23e910a8cba844a31ffe4215598160
Andy <amillion3@gmail.com>
2018-07-23 19:35:07 -0500

    refactoring helper function for geojson conersion

--- src/helperFunctions/jsonToGeoJson.js
+++ src/helperFunctions/jsonToGeoJson.js
@@ -1,76 +1,72 @@
-// "sample3": {
-//   "createdBy": "d934ks0l0slsksjdancudk37373",
-//   "createdDate": "1/1/2018",
-//   "coordLat": 36.148399,
-//   "coordLong": -86.762028,
-//   "status": "Newly Added",
-//   "severity": "Low",
-//   "descriptionNotes": "On shoulder",
-//   "updated": true,
-//   "updatedUserId": "d934ks0l0slsksjdl330128",
-//   "updatedDate": "01/02/2018"
-// }
-// -------------------------------
-// {
-//   "type": "Feature",
-//   "geometry": {
-//     "type": "Point",
-//     "coordinates": [125.6, 10.1]
-//   },
-//   "properties": {
-//     "name": "Dinagat Islands"
-//   }
-// }
+// let inputCreatedBy = '';
+// let inputCreatedDate = '';
+// let inputCoordLat = '';
+// let inputCoordLong = '';
+// let inputStatus = '';
+// let inputSeverity = '';
+// let inputDescriptionNotes = '';
+// let inputUpdated = '';
+// let inputUpdatedUserId = '';
+// let inputUpdatedDate = '';
 
-let inputCreatedBy = '';
-let inputCreatedDate = '';
-let inputCoordLat = '';
-let inputCoordLong = '';
-let inputStatus = '';
-let inputSeverity = '';
-let inputDescriptionNotes = '';
-let inputUpdated = '';
-let inputUpdatedUserId = '';
-let inputUpdatedDate = '';
-
-const conversion = () => {
-  let convertedObject = {
+const jsonToGeoJson = input => {
+  return {
     'type': 'Feature',
     'geometry': {
       'type': 'Point',
-      'coordinates': [inputCoordLat, inputCoordLong]
+      'coordinates': [input.coordLat, input.coordLong]
     },
     'properties': {
-      'createdBy': 'd934ks0l0slsksjdancudk37373',
-      'createdDate': '1/1/2018',
-      'coordLat': 36.148399,
-      'coordLong': -86.762028,
-      'status': 'Newly Added',
-      'severity': 'Low',
-      'descriptionNotes': 'On shoulder',
-      'updated': true,
-      'updatedUserId': 'd934ks0l0slsksjdl330128',
-      'updatedDate': '01/02/2018'
+      'createdBy': input.createdBy,
+      'createdDate': input.createdDate,
+      'status': input.status,
+      'severity': input.severity,
+      'descriptionNotes': input.descriptionNotes,
+      'updated': input.updated,
+      'updatedUserId': input.updatedUserId,
+      'updatedDate': input.updatedDate
     },
-  }
 };
 
-const readAndStoreValues = inputObject => {
-  inputCreatedBy = inputObject.createdBy;
-  inputCreatedDate = inputObject.createdDate;
-  inputCoordLat = inputObject.coordLat;
-  inputCoordLong = inputObject.coordLong;
-  inputStatus = inputObject.status;
-  inputSeverity = inputObject.severity;
-  inputDescriptionNotes = inputObject.descriptionNotes;
-  inputUpdated = inputObject.updated;
-  inputUpdatedUserId = inputObject.updpatedUserId;
-  inputUpdatedDate = inputObject.updatedDate;
-  conversion();
-};
+// const conversionProcess = () => {
+//   const convertedObject = {
+//     'type': 'Feature',
+//     'geometry': {
+//       'type': 'Point',
+//       'coordinates': [inputCoordLat, inputCoordLong]
+//     },
+//     'properties': {
+//       'createdBy': inputCreatedBy,
+//       'createdDate': inputCreatedDate,
+//       'coordLat': inputCoordLat,
+//       'coordLong': inputCoordLong,
+//       'status': inputStatus,
+//       'severity': inputSeverity,
+//       'descriptionNotes': inputDescriptionNotes,
+//       'updated': inputUpdated,
+//       'updatedUserId': inputUpdatedUserId,
+//       'updatedDate': inputUpdatedDate
+//     }
+//   };
+//   return convertedObject;
+// };
 
-const jsonToGeoJson = () => {
-  readAndStoreValues();
-};
+// const readAndStoreValues = inputObject => {
+//   inputCreatedBy = inputObject.createdBy;
+//   inputCreatedDate = inputObject.createdDate;
+//   inputCoordLat = inputObject.coordLat;
+//   inputCoordLong = inputObject.coordLong;
+//   inputStatus = inputObject.status;
+//   inputSeverity = inputObject.severity;
+//   inputDescriptionNotes = inputObject.descriptionNotes;
+//   inputUpdated = inputObject.updated;
+//   inputUpdatedUserId = inputObject.updpatedUserId;
+//   inputUpdatedDate = inputObject.updatedDate;
+//   conversionProcess();
+// };
+
+// const jsonToGeoJson = () => {
+//   return readAndStoreValues();
+// };
 
 export default jsonToGeoJson;
