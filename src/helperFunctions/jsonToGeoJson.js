aaa9428e008c2e1d165ec3f9f96d28e01d047848
Andy <amillion3@gmail.com>
2018-07-25 21:54:47 -0500

    WIP, the original broken code

--- src/helperFunctions/jsonToGeoJson.js
+++ src/helperFunctions/jsonToGeoJson.js
@@ -2,29 +2,14 @@ const convertedArray = [];
 
 const jsonToGeoJson = input => {
   for (let i = 0; i < input.length; i++) {
-    const abc = input[i].createdBy;
-
-    // preserve newlines, etc - use valid JSON
-    abc = abc.replace(/\\n/g, "\\n")
-      .replace(/\\'/g, "\\'")
-      .replace(/\\"/g, '\\"')
-      .replace(/\\&/g, "\\&")
-      .replace(/\\r/g, "\\r")
-      .replace(/\\t/g, "\\t")
-      .replace(/\\b/g, "\\b")
-      .replace(/\\f/g, "\\f");
-    // remove non-printable and other non-valid JSON chars
-    s = s.replace(/[\u0000-\u0019]+/g,"");
-    var o = JSON.parse(s)
-
     const convertedObject = {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": '"[' + input[i].coordLat + ',' + input[i].coordLong + ']"'
+        "coordinates": `"[${input[i].coordLat}, ${input[i].coordLong}]"`
       },
       "properties": {
-        "createdBy": '"' + abc + '"',
+        "createdBy": `"${input[i].createdBy}"`,
         "createdDate": `"${input[i].createdDate}"`,
         "status": `"${input[i].status}"`,
         "severity": `"${input[i].severity}"`,
