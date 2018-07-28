3c7d467b640568ee69e2c6cd625bbc437f8133b7
Andy <amillion3@gmail.com>
2018-07-28 13:27:10 -0500

    modal shows up, but is not functional

--- src/Components/MapMain/MapMain.js
+++ src/Components/MapMain/MapMain.js
@@ -1,11 +1,11 @@
 import React, { createRef, Component } from 'react';
 import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import {Modal, Button} from 'react-bootstrap';
 
 import GenerateMarkers from '../GenerateMarkers/GenerateMarkers';
 import MenuItemAddPothole from '../MenuItemAddPothole/MenuItemAddPothole';
 import potholeRequests from '../../firebaseRequests/potholeRequests';
-// import {getGeoJsonObject} from '../../helperFunctions/getGeoJsonObject';
-// import auth from '../../firebaseRequests/auth';
+import auth from '../../firebaseRequests/auth';
 
 import './MapMain.css';
 
@@ -17,11 +17,15 @@ class MapMain extends Component {
       potholes: [],
       hasLocation: false,
       latlng: {
-        lat: 36.1643083,
-        lng: -86.7973366,
+        lat: '',
+        lng: '',
       },
+      show: false,
     };
   }
+
+  showModal = () => this.setState({ show: true });
+  hideModal = () => this.setState({ show: false });
 
   componentWillMount () {
     potholeRequests
@@ -40,14 +44,18 @@ class MapMain extends Component {
   handleClick = e => {
     this.mapRef.current.leafletElement.locate();
 
-    // console.log(e);
+    console.log('clicked', e);
     const tempPothole = {};
     tempPothole.coordLat = e.latlng.lat;
     tempPothole.coordLong = e.latlng.lng;
     tempPothole.isComplete = false;
+    this.showModal();
+
     // need to gather more attributes here
+
     const {potholes} = this.state;
     potholes.push(tempPothole);
+
     // console.log(tempPothole);
     // Display modal/Popup
     // Display for for user input
@@ -65,13 +73,6 @@ class MapMain extends Component {
   }
 
   render () {
-    const marker = this.state.hasLocation ? (
-      <Marker position={this.state.latlng}>
-        <Popup>
-          <span>You are here</span>
-        </Popup>
-      </Marker>
-    ) : null;
     const potholeComponents = this.state.potholes.map(pothole => {
       return (
         <GenerateMarkers
@@ -82,6 +83,17 @@ class MapMain extends Component {
     });
     return (
       <div className='map-container'>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>hello</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
         <Map
           center={[36.1491592, -86.7703593]}
           zoom={10}
@@ -96,7 +108,7 @@ class MapMain extends Component {
           <div className="">
             {potholeComponents}
           </div>
-          {marker}
+          {/* {marker} */}
         </Map>
         <MenuItemAddPothole />
       </div>
