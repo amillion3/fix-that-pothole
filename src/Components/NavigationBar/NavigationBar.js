77fc562ffbe0d56004e01df5d857f90d28278371
Andy <amillion3@gmail.com>
2018-07-29 15:48:49 -0500

    WIP, redoing the entire navbar to be responsive

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 import authRequests from '../../firebaseRequests/auth';
 
@@ -31,98 +31,54 @@ class NavigationBar extends React.Component {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav>
-          {
-            authed ? (
-              // USER IS LOGGED IN
-              <Nav className="nav navbar-nav navbar-right">
-                <NavItem>
-                  <Link to='/map'>Map</Link></NavItem>
-                <NavItem>
-                  <Link to='/dashboard'>Dashboard</Link></NavItem>
-                <NavItem><Link to='/login'>
-                  <button
-                    onClick={logoutClicked}
-                  >
-                  Logout</button></Link></NavItem>
-              </Nav>
-            ) : (
-              // USER IS NOT LOGGED IN
-              <Nav className="nav navbar-nav navbar-right">
-                <NavItem><Link to='/login'>Login</Link></NavItem>
-              </Nav>
-            )
-          }
-          </Nav>
           <Nav pullRight>
-          {
-            authed ? (
+            {
+              authed ? (
               // USER IS LOGGED IN
-              <Nav className="nav navbar-nav navbar-right">
-                <NavItem>
-                  <Link to='/map'>Map</Link></NavItem>
-                <NavItem>
-                  <Link to='/dashboard'>Dashboard</Link></NavItem>
-                <NavItem><Link to='/login'>
-                  <button
-                    onClick={logoutClicked}
-                  >
-                  Logout</button></Link></NavItem>
-              </Nav>
-            ) : (
-              // USER IS NOT LOGGED IN
-              <Nav className="nav navbar-nav navbar-right">
-                <NavItem><Link to='/login'>Login</Link></NavItem>
-              </Nav>
-            )
-          }
+                <Nav className="nav navbar-nav navbar-right">
+                  <NavItem>
+                    <Link to='/map'>Map</Link></NavItem>
+                  <NavItem>
+                    <Link to='/dashboard'>Dashboard</Link></NavItem>
+                  <NavItem><Link to='/login'>
+                    <button
+                      onClick={logoutClicked}
+                    >
+                    Logout</button></Link></NavItem>
+                </Nav>
+              ) : (
+                // USER IS NOT LOGGED IN
+                <Nav className="nav navbar-nav navbar-right">
+                  <NavItem><Link to='/login'>Login</Link></NavItem>
+                </Nav>
+              )
+            }
           </Nav>
+          {/* <Nav pullRight>
+            {
+              authed ? (
+                // USER IS LOGGED IN
+                <Nav className="nav navbar-nav navbar-right">
+                  <NavItem>
+                    <Link to='/map'>Map</Link></NavItem>
+                  <NavItem>
+                    <Link to='/dashboard'>Dashboard</Link></NavItem>
+                  <NavItem><Link to='/login'>
+                    <button
+                      onClick={logoutClicked}
+                    >
+                    Logout</button></Link></NavItem>
+                </Nav>
+              ) : (
+                // USER IS NOT LOGGED IN
+                <Nav className="nav navbar-nav navbar-right">
+                  <NavItem><Link to='/login'>Login</Link></NavItem>
+                </Nav>
+              )
+            }
+          </Nav> */}
         </Navbar.Collapse>
-      </Navbar>;
-      // <Navbar className="navbar navbar-default">
-      //   <div className="container-fluid">
-      //     <div className="navbar-header">
-      //       <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-      //         <span className="sr-only">Toggle navigation</span>
-      //         <span className="icon-bar"></span>
-      //         <span className="icon-bar"></span>
-      //         <span className="icon-bar"></span>
-      //       </button>
-      //       <img className='navbar-brand navbar-img-brand'src='https://www.andymillion.com/fix-that-pothole/potholesymbol.png' alt='icon' />
-      //       {
-      //         authed ? (
-      //           <Link to='/map' className='navbar-brand'>Fix That Pothole</Link>
-      //         ) : (
-      //           <Link to='/login' className='navbar-brand'>Fix That Pothole</Link>
-      //         )
-      //       }
-
-      //     </div>
-      //     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      //       {
-      //         authed ? (
-      //           // USER IS LOGGED IN
-      //           <Nav className="nav navbar-nav navbar-right">
-      //             <NavItem>
-      //               <Link to='/map'>Map</Link></NavItem>
-      //             <NavItem>
-      //               <Link to='/dashboard'>Dashboard</Link></NavItem>
-      //             <NavItem><Link to='/login'>
-      //               <button
-      //                 onClick={logoutClicked}
-      //               >
-      //               Logout</button></Link></NavItem>
-      //           </Nav>
-      //         ) : (
-      //           // USER IS NOT LOGGED IN
-      //           <Nav className="nav navbar-nav navbar-right">
-      //             <NavItem><Link to='/login'>Login</Link></NavItem>
-      //           </Nav>
-      //         )
-      //       }
-      //     </div>
-      //   </div>
-      // </Navbar>
+      </Navbar>
     );
   }
 };
