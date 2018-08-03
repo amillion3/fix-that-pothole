315da7875796644602b34dcb62ced7aeefacf540
Andy <amillion3@gmail.com>
2018-08-02 20:17:11 -0500

    added prop types in for all relevant components

--- src/Components/PotholeCompleteRecord/PotholeCompleteRecord.js
+++ src/Components/PotholeCompleteRecord/PotholeCompleteRecord.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 import potholeRequests from '../../firebaseRequests/potholeRequests';
@@ -274,7 +275,9 @@ class PotholeCompleteRecord extends React.Component {
   }
 }
 
-// back button would be nice
-// collapse with a map marker
+PotholeCompleteRecord.propTypes = {
+  firebaseId: PropTypes.string,
+  history: PropTypes.object.isRequired,
+};
 
 export default PotholeCompleteRecord;
