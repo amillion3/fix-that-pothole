4f6480d5f9450868af6902d92fb578d25496e97f
Andy <amillion3@gmail.com>
2018-08-11 15:29:56 -0500

    added streetview link. need to update full pothole record view still

--- src/Components/DashPothole/DashPothole.js
+++ src/Components/DashPothole/DashPothole.js
@@ -15,24 +15,41 @@ class DashPothole extends React.Component {
       this.props.history.push(`/pothole/${details.id}`);
     };
 
+    const clickStreetView = () => {
+      return `
+      http://maps.google.com/maps?q=&layer=c&cbll=${details.coordLat},${details.coordLong}
+      `;
+    };
+
     return (
       <tr>
         <td>
           <button
             type="button"
-            className="btn btn-primary"
+            className="btn"
             onClick={clickSinglePothole}>
             <span className="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
             Full Record
           </button>
         </td>
+        <td>
+          <a href={clickStreetView()} target="_blank">
+            <button
+              type="button"
+              className="btn"
+            >
+              <span className="glyphicon glyphicon-camera" aria-hidden="true"></span>
+              Streetview
+            </button>
+          </a>
+        </td>
         <td className='dash-pothole-mobile'>{details.status}</td>
         <td className='dash-pothole-mobile'>{details.createdDate}</td>
         <td className='dash-pothole-mobile'>{details.severity}</td>
+        <td className='dash-pothole-mobile'>{JSON.stringify(details.updated)}</td>
         <td className='overflow-hidden'>
           {details.descriptionNotes}
         </td>
-        <td className='dash-pothole-mobile'>{JSON.stringify(details.updated)}</td>
       </tr>
     );
   }
